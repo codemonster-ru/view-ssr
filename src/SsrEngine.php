b8e15668aeee67b7ac2480338299215fe005f299
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2026-06-08 00:10:40 +0700

    Improve architecture guardrails and ecosystem readiness

--- src/SsrEngine.php
+++ src/SsrEngine.php
@@ -7,18 +7,27 @@ use Codemonster\View\EngineInterface;
 
 final class SsrEngine implements EngineInterface
 {
-    public function __construct(private object $bridge)
+    private \Closure $renderBridge;
+
+    public function __construct(object $bridge)
     {
-        if (!\method_exists($bridge, 'render')) {
+        $render = [$bridge, 'render'];
+
+        if (!\is_callable($render)) {
             throw new \InvalidArgumentException(
                 'SSR bridge must have a render(string $view, array $data = []): string method.'
             );
         }
+
+        $this->renderBridge = \Closure::fromCallable($render);
     }
 
+    /**
+     * @param array<string, mixed> $data
+     */
     public function render(string $view, array $data = []): string
     {
-        $result = $this->bridge->render($view, $data);
+        $result = ($this->renderBridge)($view, $data);
 
         if (!\is_string($result)) {
             throw new \RuntimeException('SSR bridge returned non-string response.');
